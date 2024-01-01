ff4ddf07fa602a54c38e50003f3a466df74f1262
SemblanceOfSense <semblanceofsensegaming@gmail.com>
2024-01-01 10:34:33 -0700

    New workaround thats much dumber but works better

--- src/server/Runner.ts
+++ src/server/Runner.ts
@@ -28,41 +28,12 @@ export class Runner extends Dummy {
         return getDescendantsWhichIsA(f, "Part");
     }
 
-    public getPathfindingParams(): AgentParameters {
-        return {
-            AgentRadius : 1,
-            AgentCanJump : true,
-        };
-    }
-
-    public getPaths(): Path[] {
-        let pathList = new Array<Path>;
-        for (let x of pathList) {
-            pathList.push(PathfindingService.CreatePath(this.getPathfindingParams()));
-        }
-        print("Paths fetched!");
-        return pathList;
-    }
-
-    public computePaths(pat: Path[]): void {
-        let i: number;
-        let p: Part[] = this.getParts();
-        let paths: Path[] = pat;
-        for (i = 0; i < paths.size(); i++) {
-            if (i + 1 === paths.size()) {
-                paths[i].ComputeAsync(p[i].Position, p[0].Position);
-            } else {
-                paths[i].ComputeAsync(p[i].Position, p[i + 1].Position);
-            }
-        }
-        print("computed!");
-    }
-
     public loop(): void {
         wait(7);
         while (true) {
-            this.computePaths(this.getPaths());
-            wait(1);
+            for (let x of this.getParts()) {
+                this.getHumanoid().MoveTo(x.Position);
+            }
         }
     }
 }
